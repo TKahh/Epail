f6290c9975fcd309e5596f958ba21e41fadd6ed3
YuInH <cockinglmht@gmail.com>
2024-12-10 22:18:40 +0700

    Adding Send Email in CC/BCC mode

--- lib/services/email_service.dart
+++ lib/services/email_service.dart
@@ -1,11 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EmailService {
-  Future<void> sendEmail(String fromPhone, List<String> toPhones,
-      String subject, String body) async {
+  Future<void> sendEmail(
+      String fromPhone,
+      List<String> toPhones,
+      String subject,
+      String body,
+      List<String> ccPhones,
+      List<String> bccPhones) async {
     await FirebaseFirestore.instance.collection('emails').add({
       'from': fromPhone,
       'to': toPhones,
+      'cc': ccPhones,
+      'bcc': bccPhones,
       'subject': subject,
       'body': body,
       'timestamp': FieldValue.serverTimestamp(),
